7fb68e5039c75d8f5dda88c222eabecff21e0e4d
Abdullahi Abdulwasiu <Abdullahiabdulwasiu1@gmail.com>
2021-12-18 00:25:19 +0100

    Ignore database setting

--- backend/settings.py
+++ backend/settings.py
@@ -68,6 +68,9 @@ MIDDLEWARE = [
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of 3c345ce (koko)
+=======
 >>>>>>> parent of 3c345ce (koko)
 =======
 >>>>>>> parent of 3c345ce (koko)
