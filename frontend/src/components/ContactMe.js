52849ee71dd61ea5adb1be1acce28d93b3fb97ef
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2021-12-28 01:50:59 +0100

    refactor for redeploy

--- frontend/src/components/ContactMe.js
+++ frontend/src/components/ContactMe.js
@@ -15,10 +15,21 @@ const ContactMe = () => {
         console.log(response)
      })
     
-   };
  
-   return (
-     <section className={classes.ingredientform}>
+    (function(){document.getElementById( "message").value=''})();
+    (function(){document.getElementById( "title").value=''})();
+    (function(){document.getElementById("p1").style.display='block'})();
+   };
+  
+ 
+   return (    
+
+   
+     <section className={classes.formCover}>
+        <p id="p1" style={{ display:"none", color: "green", paddingTop: "20px", font: "inherit"}}>
+       Thanks For Contacting, I Will Get Back To You Soon 
+       <span>    <i class="fa fa-thumbs-up" style={{ fontSize:"24px", color:"green"}}></i></span></p>
+       
         <div className={classes.card}>
          <form onSubmit={submitHandler}>
            <div className={classes.formcontrol}>
@@ -31,11 +42,15 @@ const ContactMe = () => {
              <textarea autoFocus rows="6" cols="100" name="comment" id="message" required value={enteredMessage} 
               onChange={event => {setEnteredMessage( event.target.value,) }}/>
            </div>
-           <div className={classes.ingredientform__actions}>
-             <button type="submit">Send Message</button>
+           <div className={classes.formform__actions}>
+             <button type="submit" onSubmit="{ClearFormHandler}">Send Message</button>
            </div>
          </form>
          </div> 
+        <div> <p style={{ color: "red", paddingTop: "20px", font: "inherit"}}>
+         You Could Also Send Me An Email Instead:
+        <a href="mailto:jibril41@yahoo.com?Subject=MyApp%20again" target="_top">Send Mail</a>
+      </p></div>
      </section>
     
    );
